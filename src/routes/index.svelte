1559fa1453d101e8223f4f306e108f1092074dee
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-05-02 18:19:15 +0300

    clean up and add Inter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,12 @@
 <svelte:head>
-	<title>Home</title>
+	<title>Rewrite: IELTS writing toolkit</title>
 </svelte:head>
 
-<section>Hello world!</section>
+<section>
+	<h1 class="hero-title">Write. Check. Correct. Repeat.</h1>
+	<h2 class="hero-subtitle">Get instant feedback and track your progress – all in one place</h2>
+	<a class="text-blue-600 underline" href="/signup">Get started now</a>
+</section>
 
 <style>
 	section {
@@ -11,5 +15,16 @@
 		justify-content: center;
 		align-items: center;
 		flex: 1;
+		color: rgba(31, 41, 55);
+	}
+
+	.hero-title {
+		font-size: 72px;
+		max-width: 512px;
+		text-align: center;
+		font-weight: bold;
+	}
+
+	.hero-subtitle {
 	}
 </style>
