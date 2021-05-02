19baea9360d2ed4e9072ce1026d4c9d703cb9a3a
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-05-02 21:13:50 +0300

    fix paper card

--- src/components/PaperCard.svelte
+++ src/components/PaperCard.svelte
@@ -4,16 +4,38 @@
 	export let paper: Paper;
 </script>
 
-<li class="hover:bg-gray-50 hover:cursor-pointer flex justify-between items-center">
+<li>
 	<a href={`/paper/${paper.id}`}>
 		<div class="flex flex-col flex-shrink-0 py-4 px-2 max-w-lg w-full">
-			<div class="font-medium truncate text-gray-800 paper-question">
+			<div class="question">
 				{paper.question}
 			</div>
-			<div class="truncate mt-2 text-gray-600 paper-body">{paper.body}</div>
+			<div class="body mt-2">{paper.body}</div>
 		</div>
-		<div class="flex font-medium text-gray-800 px-4">
+		<div class="score px-4">
 			{parseFloat(paper.overallBand.toString()).toFixed(1)}
 		</div>
 	</a>
 </li>
+
+<style lang="postcss">
+	a {
+		@apply cursor-pointer flex justify-between items-center;
+	}
+
+	a:hover {
+		@apply bg-gray-50;
+	}
+
+	.question {
+		@apply font-medium truncate text-gray-800;
+	}
+
+	.body {
+		@apply truncate text-gray-600;
+	}
+
+	.score {
+		@apply flex font-medium text-gray-800;
+	}
+</style>
