631631a2438ce9ed00e7dda79b1a585360fc11dc
Simon <2471506+simonuvarov@users.noreply.github.com>
2021-05-06 20:18:32 +0300

    add side menu

---
+++ src/components/VerticalMenu.svelte
@@ -0,0 +1,17 @@
+<script>
+	const navigation = [
+		{ name: 'Dashboard', href: '#', current: true },
+		{ name: 'Settings', href: '#', current: false }
+	];
+</script>
+
+<nav class="space-y-1" aria-label="Sidebar">
+	{#each navigation as item}
+		<a
+			href={item.href}
+			class="text-gray-600 hover:bg-gray-50 hover:text-gray-900 flex items-center px-3 py-2 text-sm font-medium rounded-md"
+		>
+			<span class="truncate">{item.name}</span>
+		</a>
+	{/each}
+</nav>
--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,6 +1,7 @@
 <script lang="typescript">
 	import { onMount } from 'svelte';
 	import PaperList from '../components/PaperList.svelte';
+	import VerticalMenu from '../components/VerticalMenu.svelte';
 	import type { Paper } from '../services/paper.service';
 	import paperService from '../services/paper.service';
 
@@ -15,6 +16,17 @@
 	<title>Dashboard</title>
 </svelte:head>
 
-<div>
-	<PaperList {papers} />
+<div class="flex h-screen overflow-hidden">
+	<aside><VerticalMenu /></aside>
+	<main><PaperList {papers} /></main>
 </div>
+
+<style lang="postcss">
+	aside {
+		@apply w-60 flex flex-col border-r overflow-auto bg-gray-50 px-4 pt-12;
+	}
+
+	main {
+		@apply flex-1 bg-white p-10 overflow-y-auto;
+	}
+</style>
